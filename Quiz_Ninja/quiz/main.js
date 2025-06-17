ebbc7175144b1198f2901b0200f443ff7400673b
Mechantchulo <chulomechant@gmail.com>
2025-06-17 22:57:59 +0300

    Update Quiz Ninja main file

--- Quiz_Ninja/quiz/main.js
+++ Quiz_Ninja/quiz/main.js
@@ -1,1 +1,35 @@
-alert('Welcome to Quiz Ninja!');
+const quiz = [
+    ["Who is the president of Kenya?", "Ruto"],
+    ["Who is the president of USA?", "Trump"],
+    ["Who is the president of Tz?", "Suluhu"]
+
+]
+
+//start the quiz
+
+function start(quiz) {
+    let score = 0;
+
+    for (const [question, answer] of quiz) {
+        const response = ask(question);
+        check(response, answer);
+
+    }
+}
+gameOver();
+
+function ask(question) {
+    return prompt(question);
+}
+
+function check(response) {
+    if (response === answer) {
+        alert('Correct!!');
+        score++;
+    } else {
+        alert('Wrong answer to the question asked!!');
+
+    }
+}
+
+start(quiz);
