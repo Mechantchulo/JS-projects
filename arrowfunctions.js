bdc530f893caedce8c622e23b4fde93d4c7898e0
Mechantchulo <chulomechant@gmail.com>
2025-06-17 22:56:35 +0300

    Add arrow functions and callback examples

--- arrowfunctions.js
+++ arrowfunctions.js
@@ -1,4 +1,24 @@
 // Denoted by =>
 //useful for short functions but get complicated when funciton block is large
 
-const square = x => x*x;
+//const square = x => x*x;
+/* 
+const square = () =>{
+
+    return x*x;
+}
+
+const squaredNum = square(4);
+
+console.log(squaredNum); 
+
+this will return that x is undefined*/
+
+const square = x =>{
+
+    return x*x;
+}
+
+const squaredNum = square(4);
+
+console.log(squaredNum);
---
+++ callbacks.js
@@ -0,0 +1,4 @@
+/* A callback is a function that is passed to another function as an argument, 
+which is then invoked inside the outer function to complete some kind of routine or action. */
+
+
